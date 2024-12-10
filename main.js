4d104fc652a66383e39cc023e20f689c688ae043
CloudTwelve <whippedreeeem@gmail.com>
2024-12-10 10:05:08 -0500

    added quotes

---
+++ main.js
@@ -0,0 +1,18 @@
+const quoteObj = {
+    "To live is the rarest thing in the world. Most people just exist.": "Oscar Wilde",
+    "You are never too old to set another goal or to dream a new dream.": "C.S. Lewis",
+    "Be stronger than your excuses.": "found on the internet",
+    ""
+};
+
+const islamicQuoteObj = {
+    "Excellence is to worship Allah as if you see Him, for if you do not see Him, He surely sees you.": "Muhammad (ﷺ), Sahih Muslim 8",
+    "Verily, Allah does not look at your appearance or wealth, but rather He looks at your hearts and actions.": "Muhammad (ﷺ), Sahih Muslim 2564",
+    "Whoever believes in Allah and the Last Day, let him speak goodness or remain silent.": "Muhammad (ﷺ), Sahih Al-Bukhari 6136",
+    "Verily, Allah will put His servant to trial in what He gives him. Whoever is pleased with what Allah has apportioned for him, then Allah will bless him in it and expand it. Whoever is not pleased, he will never be blessed in it.": "Muhammad (ﷺ), Musnad Ahmad 20279",
+    "The first to be called to Paradise are those who always praised Allah in times of prosperity and adversity.": "Al-Mu’jam al-Kabīr lil-Ṭabarānī 12345",
+    "I am amazed by optimism, the good word, the kind word.": "Muhammad (ﷺ), Sahih Muslim 2224",
+    "The son of Adam does not act with a deed better to save him from the punishment of Allah than with the remembrance of Allah.": "Muhammad (ﷺ), Musnad Aḥmad 22079",
+    "All people begin the day by trading for their soul, either setting it free or ensuring its ruin.": "Muhammad (ﷺ), Sahih Muslim 223",
+    "": "",
+}
